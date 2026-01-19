01b1c47b55a9d317f3f1db82eb038ed12f8a7a9c
KrishnaAgrawal0612 <krishnaagrawal0612@gmail.com>
2026-01-19 14:52:31 +0530

    Initial commit

---
+++ src/main/java/com/example/day2_part2/controller/home.java
@@ -0,0 +1,12 @@
+package com.example.day2_part2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class home {
+    @GetMapping("/about")
+    public String about(){
+        return "this is abt page";
+    }
+}
