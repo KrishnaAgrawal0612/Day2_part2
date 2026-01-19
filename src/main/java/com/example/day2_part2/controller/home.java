a92f101e292e07cb98867dc91691e61bc37090cd
KrishnaAgrawal0612 <krishnaagrawal0612@gmail.com>
2026-01-19 15:24:05 +0530

    Added all student details

--- src/main/java/com/example/day2_part2/controller/home.java
+++ src/main/java/com/example/day2_part2/controller/home.java
@@ -4,11 +4,19 @@ import com.example.day2_part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student=new StudentModel(1,"Krishna","krishna@gmail.com");
-        return student;
+    public List<StudentModel> getStudent(){
+        List<StudentModel> student=new ArrayList<>();
+       student.add(new StudentModel(1,"Krishna","krishna@gmail.com"));
+       student.add(new StudentModel(2,"awasthi","awasthi@gmail.com"));
+       student.add(new StudentModel(3,"ayush","ayush@gmail.com"));
+        student.add(new StudentModel(4,"Asutosh","ashutosh@gmail.com"));
+        student.add(new StudentModel(5,"Lalkesh","lalkesha@gmail.com"));
+       return student;
     }
 }
